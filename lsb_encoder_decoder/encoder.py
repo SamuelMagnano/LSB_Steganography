26c8060a1ea30046df3decb7d8ded4249395346c
SamuelMagnano <samuelmagnano@gmail.com>
2026-03-07 23:41:15 +0100

    Encoding
    Defined the encodind function

---
+++ lsb_encoder_decoder/encoder.py
@@ -0,0 +1,64 @@
+from typing import List
+import logging
+
+class Encoder:
+  image: List[List[List[str]]]
+  secret_message: str
+  mask = 11111111
+  logging.basicConfig(level=logging.INFO,
+                      format="%(asctime)s    |   %(levelname)s [%(name)s - %(funcName)s]   |   %(message)s",
+                      datefmt="%d-%m-%Y %H:%M:%S",
+                      filename="LSB_Steganography.log")
+  
+  def set_secret_message(self) -> bool:
+    message = ""
+    value = input("1. Define the message\nDrag and drop the file otherwise\n")
+    if value.strip() == "1":
+      self.secret_message = input()
+      logging.info("Secret messagge loaded correctly!")
+      return True
+    else: #read file and use the content as the secret_message
+      ...
+    logging.warning("Error while loading the secret message!")
+    return False
+   
+  def set_image(self, image: List[List[List[str]]]) -> bool:
+    if image != None:
+      self.image = image
+      return True
+    return False
+  
+  #Message -> str -> Ascii -> 8 bit binary
+  def message_to_bin(self, secret_message:str) -> str:
+    bin_message: str = ""
+    for char in secret_message:
+      bin_message += bin(ord(char)).split("b")[1].zfill(8)
+    logging.info(f"Binary message: {bin_message}")
+    return bin_message
+  
+  def encode(self) -> List[List[List[str]]]:
+    if self.secret_message == "": 
+      logging.warning("Secret message empty!")
+      raise Exception("Error while encoding: Secret message empty!")
+    logging.info("Converting the secret message to binary (UTF-8)")
+    bin_message = self.message_to_bin(self.secret_message)
+    logging.info("Conversion done!")
+    cont = 0
+    logging.info("Encoding the secret message inside the image")
+    for row in range(len(self.image)):
+      for rgb_channel in range(len(self.image[row])):
+        for idx,value in enumerate(self.image[row][rgb_channel]):
+          #print(f"original value: {value}, bin: {bin(value)}, 8_bit_bin: {bin(value).split('b')[1].zfill(8)}, 8_bit_bin_to_int: {int(bin(value).split('b')[1].zfill(8),2)}")
+          if len(bin_message)<=0: continue
+          x = bin_message[0]
+          bin_message = bin_message[1:]
+          result = "0b" + bin(int(self.image[row][rgb_channel][idx],2) & int(f"0b1111111{x}",2)).split("b")[1].zfill(8)
+          self.image[row][rgb_channel][idx] = result
+          cont += 1
+    logging.info(f"Evaluated {cont} RGB channel values")
+    logging.info("Encoding completed!")
+    if cont>=0:
+      return self.image 
+    else: 
+      logging.error("Error while encoding")
+      raise Exception("Error while encoding")
--- main.py
+++ main.py
@@ -1,2 +1,49 @@
+from PIL import Image
+import numpy as np
+from typing import List, Any
+import logging
+from lsb_encoder_decoder.encoder import Encoder
+
+def logging_config() -> None:  
+  logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s    |   %(levelname)s [%(name)s - %(funcName)s]   |   %(message)s",
+                        datefmt="%d-%m-%Y %H:%M:%S",
+                        filename="LSB_Steganography.log")
+
+def image_to_bits(image: List[List[List[Any]]]) -> List[List[List[str]]]:
+  cont = 0
+  for row in range(len(image)):
+    for rgb_channel in range(len(image[row])):
+      for idx,value in enumerate(image[row][rgb_channel]):
+        #print(f"original value: {value}, bin: {bin(value)}, 8_bit_bin: {bin(value).split('b')[1].zfill(8)}, 8_bit_bin_to_int: {int(bin(value).split('b')[1].zfill(8),2)}")
+        image[row][rgb_channel][idx] = "0b" + bin(value).split('b')[1].zfill(8)
+        cont += 1
+  logging.info(f"Evaluated {cont} RGB channel values")
+  return image
+  
+
 if __name__ == "__main__":
   
+  logging_config()
+  
+  #List[List[List[int]]] -> List[riga1[pixel1, pixel2,..., pixel344] dove pixel = [R,G,B]
+  pixels = np.array(Image.open("test_image.jpg"))
+  logging.info(f"Calling image_to_bits")
+  bit_pixels = image_to_bits(pixels.tolist())
+  logging.info(f"image_to_bits completed!")
+  del pixels
+  print(bit_pixels[0][0:6])
+  
+  encoder = Encoder()
+  if encoder.set_image(bit_pixels):
+    logging.info("Image correctly setted inside the Encoder")
+    if encoder.set_secret_message(): 
+      logging.info("Secret message correctly setted inside the Encoder")
+      try:
+        encoded_image = encoder.encode()
+        logging.info("Secret message correctly encoded inside the image")
+      except Exception:
+        logging.error("An error occurred while encoding the secret message inside the given image")
+        exit()
+  
+  print(encoded_image[0][0:6])
